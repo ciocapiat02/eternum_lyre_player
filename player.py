b772aea62e70c488aa148ecc326fafd10edeb8d1
ciocapiat02 <ciocapiat02@gmail.com>
2026-02-03 13:49:45 +0100

    map note to keys
    Signed-off-by: ciocapiat02 <ciocapiat02@gmail.com>

--- player.py
+++ player.py
@@ -1,10 +1,22 @@
 import mido
 
+note_keys_map = {
+    "67": "q",
+    "69": "w",
+    "71": "e",
+    "72": "r",
+    "74": "t",
+    "76": "y",
+    "78": "u",
+    "79": "i",
+    "81": "o",
+}
+
 midifile = mido.MidiFile("test.mid")
+
 for msg in midifile.play():
     try:
-        print(msg.type)
-        print(msg.note)
-        print(msg.velocity)
+        if msg.type == "note_on":
+            print(note_keys_map[str(msg.note)])
     except Exception as e:
         pass
