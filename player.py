056ca9b80d6c112b8cdd0c30f00b5db35ad7076c
ciocapiat02 <ciocapiat02@gmail.com>
2026-02-03 13:36:31 +0100

    access message values
    Signed-off-by: ciocapiat02 <ciocapiat02@gmail.com>

--- player.py
+++ player.py
@@ -2,4 +2,9 @@ import mido
 
 midifile = mido.MidiFile("test.mid")
 for msg in midifile.play():
-    print(msg)
+    try:
+        print(msg.type)
+        print(msg.note)
+        print(msg.velocity)
+    except Exception as e:
+        pass
